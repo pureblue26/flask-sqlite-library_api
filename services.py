dd4ab2bdcdd7466a0de006277b9be49e0f44cad1
pureblue26 <2791641729@qq.com>
2026-08-23 10:59:48 +0800

    feat: 业务层（借书/还书/查书 + 边界异常）

---
+++ services.py
@@ -0,0 +1,53 @@
+
+from models import (
+    Book,
+    BookStatus,
+    BookNotFoundError,
+    BookUnavailableError,
+    BookNotBorrowedError,
+)
+import database
+
+def borrow_book(book_id: int) -> Book:
+    
+    books = database.get_book_by_id(book_id)
+    if books is not None:
+        if books.status == BookStatus.BORROWED:
+            raise BookUnavailableError(f"id = {(book_id)}书籍已经借出")
+        else:
+            database.update_book_status(book_id,BookStatus.BORROWED)
+            return  database.get_book_by_id(book_id)
+    else:
+        raise BookNotFoundError(f"id = {(book_id)}书籍不存在")
+
+
+
+def return_book(book_id: int) -> Book:
+
+    books = database.get_book_by_id(book_id)
+    if books is not None:
+        if books.status == BookStatus.AVAILABLE:
+            raise BookNotBorrowedError(f"id = {(book_id)}书籍还未借出")
+        else:
+            database.update_book_status(book_id,BookStatus.AVAILABLE)
+            return  database.get_book_by_id(book_id)
+    else:
+        raise BookNotFoundError(f"id = {(book_id)}书籍不存在")
+
+
+
+def get_book(book_id: int)->Book:
+    
+    book = database.get_book_by_id(book_id)
+    if book is not None:
+        return book
+    else:
+        raise BookNotFoundError(f"id = {(book_id)}书籍不存在")
+
+
+
+def get_books()->list[Book]:
+    return database.get_all_books()
+
+if __name__ == "__main__":
+    books = get_books()
