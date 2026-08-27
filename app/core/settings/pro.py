5975a65bd2d02f062e16ec29a4c1119dd7e22fa5
pureblue26 <2791641729@qq.com>
2026-08-27 21:58:56 +0800

    chore: 删除重复的 pro.py（prod.py 为准）

--- app/core/settings/pro.py
+++
@@ -1,7 +0,0 @@
-from app.core.settings.settings import BaseSettings 
-import os
-
-class ProdSettings(BaseSettings):
-    DEBUG = False                 
-    DB_NAME = "library_prod"
-    SECRET_KEY = os.getenv("SECRET_KEY")   
